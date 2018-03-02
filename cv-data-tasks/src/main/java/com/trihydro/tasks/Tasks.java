0c7420fc7065713e901b14e95fa1636a0ff7dcd7
Kim Perry <kimperry18@gmail.com>
2018-03-02 16:47:40 -0700

    New tasks project

---
+++ cv-data-tasks/src/main/java/com/trihydro/tasks/Tasks.java
@@ -0,0 +1,9 @@
+package com.trihydro.tasks;
+
+
+public class Tasks {
+	
+	public static void main( String[] args ) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();		
+	}
+}
---
+++ cv-data-tasks/src/main/java/com/trihydro/tasks/activetims/ActiveTimTasks.java
@@ -0,0 +1,11 @@
+package com.trihydro.tasks.activetims;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActiveTimTasks {
+    
+  
+	
+}

db9bc39f4ef1324cfd2226632a8add2a9c0b73e5
TRIHYDRO\bpayne <bpayne@trihydro.com>
2019-10-25 12:59:31 -0600

    Adding logging for ode-wrapper calls

---
+++ cv-data-service-library/src/main/java/com/trihydro/library/tables/LoggingTables.java
@@ -0,0 +1,18 @@
+package com.trihydro.library.tables;
+
+import java.util.*;
+
+public class LoggingTables extends OracleTables {
+    private static List<String> httpLoggingTable;
+
+    public static List<String> getHttpLoggingTable() {
+        if (httpLoggingTable == null) {
+            httpLoggingTable = new ArrayList<String>();
+
+            httpLoggingTable.add("REQUEST_TIME");
+            httpLoggingTable.add("REST_REQUEST");
+        }
+
+        return httpLoggingTable;
+    }
+}
--- ode-wrapper/src/main/java/com/trihydro/odewrapper/spring/ApplicationConfig.java
+++ ode-wrapper/src/main/java/com/trihydro/odewrapper/spring/ApplicationConfig.java
@@ -1,14 +1,25 @@
 package com.trihydro.odewrapper.spring;
 
+import com.trihydro.odewrapper.helpers.ApiInterceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = { "com.trihydro.odewrapper.controller" })
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    ApiInterceptor apiInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiInterceptor);
+     }
 
     public ApplicationConfig() {
         super();

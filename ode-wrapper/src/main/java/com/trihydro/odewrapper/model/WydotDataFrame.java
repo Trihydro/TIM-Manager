42d63c9766a18c7e6f0d3857e938a0876c9ac141
TRIHYDRO\bpayne <bpayne@trihydro.com>
2019-11-27 13:54:38 -0700

    Adding serialVersionUID to satisfy warnings

--- ode-wrapper/src/main/java/com/trihydro/odewrapper/model/WydotDataFrame.java
+++ ode-wrapper/src/main/java/com/trihydro/odewrapper/model/WydotDataFrame.java
@@ -1,9 +1,11 @@
 package com.trihydro.odewrapper.model;
 
-import us.dot.its.jpo.ode.plugin.j2735.OdeTravelerInformationMessage;
 import com.trihydro.library.model.ItisCode;
 
+import us.dot.its.jpo.ode.plugin.j2735.OdeTravelerInformationMessage;
+
 public class WydotDataFrame extends OdeTravelerInformationMessage.DataFrame {
+	private static final long serialVersionUID = -207518977919223715L;
 	private ItisCode[] itisCodes;
 
 	public ItisCode[] getItisCodes() {

3fe4142dac78473ccf2144de3a21e0809e8c7456
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-01-30 09:25:45 -0700

    Removing dead code

--- cv-data-service-library/src/main/java/com/trihydro/library/service/MilepostService.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/service/MilepostService.java
@@ -8,22 +8,6 @@ import com.trihydro.library.model.Milepost;
 import org.springframework.http.ResponseEntity;
 
 public class MilepostService extends CvDataServiceLibrary {
-
-	// select all mileposts
-	public static List<Milepost> selectAll() {
-		String url = String.format("/%s/mileposts", CVRestUrl);
-		ResponseEntity<Milepost[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
-				Milepost[].class);
-		return Arrays.asList(response.getBody());
-	}
-
-	public static List<Milepost> getMilepostsRoute(String route, Boolean mod) {
-		String url = String.format("/%s/mileposts-route/%s/%b", CVRestUrl, route, mod);
-		ResponseEntity<Milepost[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
-				Milepost[].class);
-		return Arrays.asList(response.getBody());
-	}
-
 	/**
 	 * Calls out to the cv-data-controller REST service to select all mileposts
 	 * within a range in one direction
@@ -38,33 +22,6 @@ public class MilepostService extends CvDataServiceLibrary {
 			Double toMilepost) {
 		String url = String.format("/%s/get-milepost-range/%s/%d/%d/%s", CVRestUrl, direction, fromMilepost, toMilepost,
 				route);
-		ResponseEntity<Milepost[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
-				Milepost[].class);
-		return Arrays.asList(response.getBody());
-	}
-
-	// select all mileposts within a range in one direction
-	public static List<Milepost> selectMilepostTestRange(String direction, String route, Double fromMilepost,
-			Double toMilepost) {
-		String url = String.format("/%s/get-milepost-test-range/%s/%d/%d/%s", CVRestUrl, direction, fromMilepost,
-				toMilepost, route);
-		ResponseEntity<Milepost[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
-				Milepost[].class);
-		return Arrays.asList(response.getBody());
-	}
-
-	// select all mileposts within a range in one direction
-	public static List<Milepost> selectMilepostRangeNoDirection(String route, Double fromMilepost, Double toMilepost) {
-		String url = String.format("/%s/get-milepost-range-no-direction/%d/%d/%s", CVRestUrl, fromMilepost, toMilepost,
-				route);
-		ResponseEntity<Milepost[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
-				Milepost[].class);
-		return Arrays.asList(response.getBody());
-	}
-
-	// select all mileposts
-	public static List<Milepost> selectAllTest() {
-		String url = String.format("/%s/mileposts-test", CVRestUrl);
 		ResponseEntity<Milepost[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
 				Milepost[].class);
 		return Arrays.asList(response.getBody());

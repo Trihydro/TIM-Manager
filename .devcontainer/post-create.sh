2817a763d47fd02ca8ce16434e7cc2b257dfb87f
Trevor Trouchon <ttrouchon@trihydro.com>
2020-02-03 22:33:58 +0000

    Updated ode jar versions from 1.0.4 to 1.1.0

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,9 +1,9 @@
 ODE_DEP_DIR=/workspaces/WyoCV/resources
 
 # Install jpo-ode JARs to maven repository
-mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-plugins-1.0.4.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-plugins -Dversion="1.0.4" -Dpackaging=jar
-mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-core-1.0.4.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-core -Dversion="1.0.4" -Dpackaging=jar
-mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-common-1.0.4.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-common -Dversion="1.0.4" -Dpackaging=jar
-mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-svcs.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-svcs -Dversion="1.0.4" -Dpackaging=jar
+mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-plugins-1.1.0.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-plugins -Dversion="1.1.0" -Dpackaging=jar
+mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-core-1.1.0.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-core -Dversion="1.1.0" -Dpackaging=jar
+mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-common-1.1.0.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-common -Dversion="1.1.0" -Dpackaging=jar
+mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-svcs.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-svcs -Dversion="1.1.0" -Dpackaging=jar
 mvn install:install-file -Dfile="${ODE_DEP_DIR}/ojdbc8.jar" -DgroupId="com.oracle" -DartifactId=ojdbc8 -Dversion="12.2.0.1.0" -Dpackaging=jar
 mvn install:install-file -Dfile="${ODE_DEP_DIR}/ucp.jar" -DgroupId="com.oracle" -DartifactId=ucp -Dversion="12.2.0.1.0" -Dpackaging=jar

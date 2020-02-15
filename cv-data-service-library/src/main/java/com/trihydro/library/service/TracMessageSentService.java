4dc32ffffc3acf0ea703cc3865326f6712b7bd7a
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-02-15 11:14:46 +0000

    TracMessageSent service unit tests

--- cv-data-service-library/src/main/java/com/trihydro/library/service/TracMessageSentService.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/service/TracMessageSentService.java
@@ -16,13 +16,13 @@ import org.springframework.stereotype.Component;
 public class TracMessageSentService extends CvDataServiceLibrary {
 
 	public List<String> selectPacketIds() {
-		String url = String.format("/%s/trac-message/packet-ids", CVRestUrl);
+		String url = String.format("%s/trac-message/packet-ids", CVRestUrl);
 		ResponseEntity<String[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url, String[].class);
 		return Arrays.asList(response.getBody());
 	}
 
 	public Long insertTracMessageSent(TracMessageSent tracMessageSent) {
-		String url = String.format("/%s/trac-message/add-trac-message-sent", CVRestUrl);
+		String url = String.format("%s/trac-message/add-trac-message-sent", CVRestUrl);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<TracMessageSent> entity = new HttpEntity<TracMessageSent>(tracMessageSent, headers);

63dd8ebda3e76ff0b2a94a6f6b8d73a611223504
Trevor Trouchon <ttrouchon@trihydro.com>
2020-02-18 21:34:55 +0000

    added workaround for git auth issue inside container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,3 +7,7 @@ mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-common-1.1.0.jar" -Dgrou
 mvn install:install-file -Dfile="${ODE_DEP_DIR}/jpo-ode-svcs.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-svcs -Dversion="1.1.0" -Dpackaging=jar
 mvn install:install-file -Dfile="${ODE_DEP_DIR}/ojdbc8.jar" -DgroupId="com.oracle" -DartifactId=ojdbc8 -Dversion="12.2.0.1.0" -Dpackaging=jar
 mvn install:install-file -Dfile="${ODE_DEP_DIR}/ucp.jar" -DgroupId="com.oracle" -DartifactId=ucp -Dversion="12.2.0.1.0" -Dpackaging=jar
+
+# Starting in vscode 1.42.0, there appears to be an issue w/ git authentication
+# Workaround until https://github.com/microsoft/vscode-remote-release/issues/2340 is resolved:
+ln -s "$(ls ~/.vscode-server/bin/* -dt | head -1)/node" /usr/local/bin

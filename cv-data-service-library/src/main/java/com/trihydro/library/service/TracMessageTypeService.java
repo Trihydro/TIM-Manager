d88bd3eb26faf64c219f3173e4002ad8ab916c89
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-02-18 17:43:26 +0000

    TracMessageTypeService unit tests

--- cv-data-service-library/src/main/java/com/trihydro/library/service/TracMessageTypeService.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/service/TracMessageTypeService.java
@@ -12,8 +12,7 @@ import org.springframework.stereotype.Component;
 public class TracMessageTypeService extends CvDataServiceLibrary {
 
 	public List<TracMessageType> selectAll() {
-
-		String url = String.format("/%s/trac-message-type/GetAll", CVRestUrl);
+		String url = String.format("%s/trac-message-type/GetAll", CVRestUrl);
 		ResponseEntity<TracMessageType[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
 				TracMessageType[].class);
 		return Arrays.asList(response.getBody());

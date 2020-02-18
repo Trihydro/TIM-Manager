485ba81713a8c78b44ad36b7b7aef1baf4b9c5e8
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-02-18 22:47:08 +0000

    LoggingService unit test

--- cv-data-service-library/src/main/java/com/trihydro/library/service/LoggingService.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/service/LoggingService.java
@@ -1,7 +1,5 @@
 package com.trihydro.library.service;
 
-import java.sql.Timestamp;
-
 import com.trihydro.library.model.HttpLoggingModel;
 
 import org.springframework.http.HttpEntity;
@@ -13,13 +11,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LoggingService extends CvDataServiceLibrary {
-    
-    public Long LogHttpRequest(String request, Timestamp requestTime, Timestamp responseTime) {
+
+    public Long LogHttpRequest(HttpLoggingModel httpLoggingModel) {
         String url = String.format("%s/http-logging/add-http-logging", CVRestUrl);
-        HttpLoggingModel httpLoggingModel = new HttpLoggingModel();
-        httpLoggingModel.setRequest(request);
-        httpLoggingModel.setRequestTime(requestTime);
-        httpLoggingModel.setResponseTime(responseTime);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<HttpLoggingModel> entity = new HttpEntity<HttpLoggingModel>(httpLoggingModel, headers);

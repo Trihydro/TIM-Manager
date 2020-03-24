f1652cd68607d142be91214daea35c2495c8e7ab
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-03-24 16:21:21 +0000

    Deprecated removals

--- ode-wrapper/src/main/java/com/trihydro/odewrapper/spring/WebConfig.java
+++ ode-wrapper/src/main/java/com/trihydro/odewrapper/spring/WebConfig.java
@@ -1,15 +1,14 @@
 package com.trihydro.odewrapper.spring;
 
 import org.springframework.context.annotation.ComponentScan;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.trihydro.odewrapper.spring")
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     public WebConfig() {
         super();

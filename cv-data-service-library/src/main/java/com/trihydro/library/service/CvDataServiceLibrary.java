b3c1eb86bd5904d11fb07d950be6f182f1cb79b7
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-03-28 12:25:56 +0000

    Code cleanup

--- cv-data-service-library/src/main/java/com/trihydro/library/service/CvDataServiceLibrary.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/service/CvDataServiceLibrary.java
@@ -1,15 +1,8 @@
 package com.trihydro.library.service;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class CvDataServiceLibrary {
 
     protected static String CVRestUrl;
-
-    // public static void setConfig(ConfigProperties config) {
-    //     DbUtility.setConfig(config);
-    // }
 
     public static void setCVRestUrl(String url) {
         CVRestUrl = url;

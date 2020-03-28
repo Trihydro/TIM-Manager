cd3ee4a296fbdec299c0d96a5388092f6635bbc2
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-03-28 16:33:36 +0000

    Added call to new REST endpoint

--- cv-data-service-library/src/main/java/com/trihydro/library/service/ActiveTimHoldingService.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/service/ActiveTimHoldingService.java
@@ -1,5 +1,8 @@
 package com.trihydro.library.service;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.trihydro.library.model.ActiveTimHolding;
 
 import org.springframework.http.HttpEntity;
@@ -12,13 +15,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ActiveTimHoldingService extends CvDataServiceLibrary {
 
-    public long insertActiveTimHolding(ActiveTimHolding activeTimHolding){
-        String url = String.format("%s/active-tim-holding/add", CVRestUrl);
+	public long insertActiveTimHolding(ActiveTimHolding activeTimHolding) {
+		String url = String.format("%s/active-tim-holding/add", CVRestUrl);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<ActiveTimHolding> entity = new HttpEntity<ActiveTimHolding>(activeTimHolding, headers);
 		ResponseEntity<Long> response = RestTemplateProvider.GetRestTemplate().exchange(url, HttpMethod.POST, entity,
 				Long.class);
 		return response.getBody();
-    }
+	}
+
+	public List<ActiveTimHolding> getActiveTimHoldingForRsu(String ipv4Address) {
+		String url = String.format("%s/active-tim-holding/get-rsu/%s", CVRestUrl, ipv4Address);
+		ResponseEntity<ActiveTimHolding[]> response = RestTemplateProvider.GetRestTemplate().getForEntity(url,
+				ActiveTimHolding[].class);
+		return Arrays.asList(response.getBody());
+	}
 }

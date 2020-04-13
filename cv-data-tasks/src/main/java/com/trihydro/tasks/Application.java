6f5d643196be813039534d99cd79dd7f1e612c06
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-04-13 22:11:47 +0000

    Adding RestTemplateProvider to imported list

--- cv-data-tasks/src/main/java/com/trihydro/tasks/Application.java
+++ cv-data-tasks/src/main/java/com/trihydro/tasks/Application.java
@@ -11,6 +11,7 @@ import com.trihydro.library.helpers.EmailHelper;
 import com.trihydro.library.helpers.JavaMailSenderImplProvider;
 import com.trihydro.library.helpers.Utility;
 import com.trihydro.library.service.ActiveTimService;
+import com.trihydro.library.service.RestTemplateProvider;
 import com.trihydro.library.service.RsuDataService;
 import com.trihydro.library.service.SdwService;
 import com.trihydro.tasks.actions.CleanupActiveTims;
@@ -26,7 +27,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({ SdwService.class, Utility.class, EmailHelper.class, JavaMailSenderImplProvider.class, ActiveTimService.class,
-                RsuDataService.class })
+                RsuDataService.class, RestTemplateProvider.class })
 public class Application {
         protected static DataTasksConfiguration config;
 

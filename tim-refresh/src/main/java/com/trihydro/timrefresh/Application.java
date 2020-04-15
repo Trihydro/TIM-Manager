37ad4c7d4f89a2ffd2dd180cb560d5207ae8d970
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-04-15 19:13:41 +0000

    Code cleanup

--- tim-refresh/src/main/java/com/trihydro/timrefresh/Application.java
+++ tim-refresh/src/main/java/com/trihydro/timrefresh/Application.java
@@ -12,11 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 
-    // @Autowired
-    // public void setConfiguration(TimRefreshConfiguration configurationRhs) {
-    //     CvDataServiceLibrary.setCVRestUrl(configurationRhs.getCvRestService());
-    // }
-
     public static void main(String[] args) {
         System.out.println("Starting TIM Refresh application at " + dateFormat.format(new Date()));
         SpringApplication.run(Application.class, args);

ca170425003c750f6bfb24f7032ba0c7f711f5d3
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-04-16 14:07:47 +0000

    Switched to StrictStubs runner

--- ode-wrapper/src/test/java/com/trihydro/odewrapper/helpers/HttpLoggingFilterTest.java
+++ ode-wrapper/src/test/java/com/trihydro/odewrapper/helpers/HttpLoggingFilterTest.java
@@ -26,9 +26,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner.StrictStubs;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(StrictStubs.class)
 public class HttpLoggingFilterTest {
     @Mock
     HttpServletRequest mockHttpServletRequest;

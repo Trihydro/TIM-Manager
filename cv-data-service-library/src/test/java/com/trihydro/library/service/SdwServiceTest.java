ede9d5c45ba1f7727e06a80e1c505220a77c53fa
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-04-20 16:47:30 +0000

    Removing test configuration in favor of mock SdwProps

--- cv-data-service-library/src/test/java/com/trihydro/library/service/SdwServiceTest.java
+++ cv-data-service-library/src/test/java/com/trihydro/library/service/SdwServiceTest.java
@@ -24,11 +24,11 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.trihydro.library.helpers.TestConfig;
 import com.trihydro.library.helpers.Utility;
 import com.trihydro.library.model.AdvisorySituationDataDeposit;
 import com.trihydro.library.model.SDXDecodeRequest;
 import com.trihydro.library.model.SDXDecodeResponse;
+import com.trihydro.library.model.SdwProps;
 import com.trihydro.library.model.SemiDialogID;
 
 import org.hamcrest.collection.IsMapContaining;
@@ -51,7 +51,7 @@ import org.springframework.web.client.RestClientException;
 public class SdwServiceTest extends BaseServiceTest {
 
     @Mock
-    TestConfig mockConfig;
+    SdwProps mockConfig;
 
     @Mock
     HttpURLConnection mockUrlConn;

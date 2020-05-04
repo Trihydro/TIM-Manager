23d00785bb1b5f7bda816efaaf7f3884ea70ea35
Trevor Trouchon <ttrouchon@trihydro.com>
2020-05-04 16:27:49 +0000

    Fixed security-result-code-type/get-all HTTP method

--- cv-data-controller/src/main/java/com/trihydro/cvdatacontroller/controller/SecurityResultCodeTypeController.java
+++ cv-data-controller/src/main/java/com/trihydro/cvdatacontroller/controller/SecurityResultCodeTypeController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("security-result-code-type")
 public class SecurityResultCodeTypeController extends BaseController {
-	@RequestMapping(value = "/get-all", method = RequestMethod.POST, headers = "Accept=application/json")
+	@RequestMapping(value = "/get-all", method = RequestMethod.GET, headers = "Accept=application/json")
 	public ResponseEntity<List<SecurityResultCodeType>> GetSecurityResultCodeTypes() {
 
 		SecurityResultCodeType securityResultCodeType = null;

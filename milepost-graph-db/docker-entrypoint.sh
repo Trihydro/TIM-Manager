cb3abd6f16febcfb25cc6609b4151be01b7f2dea
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-05-12 17:33:06 +0000

    Cypher update, neo4j config updates, adding 'I' direction

--- milepost-graph-db/docker-entrypoint.sh
+++ milepost-graph-db/docker-entrypoint.sh
@@ -354,6 +354,7 @@ declare -A ENTERPRISE
 COMMUNITY=(
      [dbms.tx_log.rotation.retention_policy]="100M size"
      [dbms.memory.pagecache.size]="2G"
+     [dbms.memory.heap.initial_size]="2G"
      [dbms.memory.heap.max_size]="2G"
      [dbms.connectors.default_listen_address]="0.0.0.0"
      [dbms.connector.https.listen_address]="0.0.0.0:7473"

c403ee30885154350ded286fb18b2cab75e01620
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-05-14 17:00:44 +0000

    ODE wrapper startup bug fix #5381

--- ode-wrapper/src/main/java/com/trihydro/odewrapper/spring/ApplicationConfig.java
+++ ode-wrapper/src/main/java/com/trihydro/odewrapper/spring/ApplicationConfig.java
@@ -1,14 +1,36 @@
 package com.trihydro.odewrapper.spring;
 
-import org.springframework.context.annotation.ComponentScan;
+import com.trihydro.library.helpers.EmailHelper;
+import com.trihydro.library.helpers.JavaMailSenderImplProvider;
+import com.trihydro.library.helpers.MilepostReduction;
+import com.trihydro.library.helpers.Utility;
+import com.trihydro.library.service.ActiveTimHoldingService;
+import com.trihydro.library.service.ActiveTimService;
+import com.trihydro.library.service.IncidentChoicesService;
+import com.trihydro.library.service.ItisCodeService;
+import com.trihydro.library.service.LoggingService;
+import com.trihydro.library.service.MilepostService;
+import com.trihydro.library.service.OdeService;
+import com.trihydro.library.service.RestTemplateProvider;
+import com.trihydro.library.service.RsuService;
+import com.trihydro.library.service.SdwService;
+import com.trihydro.library.service.TimRsuService;
+import com.trihydro.library.service.TimService;
+import com.trihydro.library.service.TimTypeService;
+import com.trihydro.odewrapper.service.WydotTimService;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackages = { "com.trihydro.odewrapper.controller", "com.trihydro.library.helpers",
-        "com.trihydro.library.service", "com.trihydro.library.tables" })
+@Import({ ActiveTimService.class, ActiveTimHoldingService.class, OdeService.class, SdwService.class,
+        TimTypeService.class, MilepostService.class, TimRsuService.class, WydotTimService.class, RsuService.class,
+        TimService.class, ItisCodeService.class, IncidentChoicesService.class, MilepostReduction.class,
+        RestTemplateProvider.class, LoggingService.class, Utility.class, EmailHelper.class,
+        JavaMailSenderImplProvider.class })
 public class ApplicationConfig implements WebMvcConfigurer {
     public ApplicationConfig() {
         super();

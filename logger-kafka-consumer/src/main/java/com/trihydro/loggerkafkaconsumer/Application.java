306f2f87febd67067d4f356eb48dd542fe99c02c
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-05-14 17:19:27 +0000

    Swapping consumer component scan for import

--- logger-kafka-consumer/src/main/java/com/trihydro/loggerkafkaconsumer/Application.java
+++ logger-kafka-consumer/src/main/java/com/trihydro/loggerkafkaconsumer/Application.java
@@ -1,13 +1,23 @@
 package com.trihydro.loggerkafkaconsumer;
 
+import com.trihydro.library.helpers.DbInteractions;
+import com.trihydro.library.helpers.EmailHelper;
+import com.trihydro.library.helpers.JavaMailSenderImplProvider;
+import com.trihydro.library.helpers.JsonToJavaConverter;
+import com.trihydro.library.helpers.SQLNullHandler;
+import com.trihydro.library.helpers.Utility;
+import com.trihydro.library.tables.BsmOracleTables;
+import com.trihydro.library.tables.DriverAlertOracleTables;
+import com.trihydro.library.tables.TimOracleTables;
 import com.trihydro.loggerkafkaconsumer.config.LoggerConfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@ComponentScan({"com.trihydro.loggerkafkaconsumer","com.trihydro.library.helpers", "com.trihydro.library.tables"})
+@Import({ Utility.class, DbInteractions.class, JsonToJavaConverter.class, TimOracleTables.class, BsmOracleTables.class,
+		SQLNullHandler.class, DriverAlertOracleTables.class, EmailHelper.class, JavaMailSenderImplProvider.class })
 @SpringBootApplication
 @EnableConfigurationProperties(LoggerConfiguration.class)
 public class Application {

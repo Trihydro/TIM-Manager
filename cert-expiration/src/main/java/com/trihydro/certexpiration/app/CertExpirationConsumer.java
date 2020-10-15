60b94df15d207a13d3e8db7ff0655b1cc6eda19f
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-10-15 19:58:51 +0000

    Adding better logging

--- cert-expiration/src/main/java/com/trihydro/certexpiration/app/CertExpirationConsumer.java
+++ cert-expiration/src/main/java/com/trihydro/certexpiration/app/CertExpirationConsumer.java
@@ -52,7 +52,7 @@ public class CertExpirationConsumer {
 		KafkaConsumer<String, String> stringConsumer = new KafkaConsumer<String, String>(consumerProps);
 		String consumerTopic = configProperties.getDepositTopic();
 		stringConsumer.subscribe(Arrays.asList(consumerTopic));
-		System.out.println("Subscribed to topic " + consumerTopic);
+		utility.logWithDate("Subscribed to topic " + consumerTopic);
 
 		Gson gson = new Gson();
 
@@ -61,13 +61,21 @@ public class CertExpirationConsumer {
 				Duration polTime = Duration.ofMillis(100);
 				ConsumerRecords<String, String> records = stringConsumer.poll(polTime);
 				for (ConsumerRecord<String, String> record : records) {
+					utility.logWithDate(String.format("Consumed from expiration topic: %s", record.value()));
 					// ok, we have a new record now:
 					// use packetId, and startDateTime to locate a unique record
 					// update expiration value
 
 					// parse record.value() as CertExpirationModel
 					var cem = gson.fromJson(record.value(), CertExpirationModel.class);
-					ats.updateActiveTimExpiration(cem.getPacketID(), cem.getStartDateTime(), cem.getExpirationDate());
+					var success = ats.updateActiveTimExpiration(cem.getPacketID(), cem.getStartDateTime(),
+							cem.getExpirationDate());
+
+					if (success) {
+						utility.logWithDate("Successfully updated expiration date");
+					} else {
+						utility.logWithDate(String.format("Failed to update expiration for data: %s", record.value()));
+					}
 				}
 			}
 		} catch (Exception ex) {

8e05c25cde5d54b519e872a9e2e5ba3485e3541d
TRIHYDRO\bpayne <bpayne@trihydro.com>
2020-10-21 21:31:41 +0000

    Comment update

--- cv-data-controller/src/main/java/com/trihydro/cvdatacontroller/controller/ActiveTimController.java
+++ cv-data-controller/src/main/java/com/trihydro/cvdatacontroller/controller/ActiveTimController.java
@@ -82,9 +82,9 @@ public class ActiveTimController extends BaseController {
 			selectStatement += " LEFT JOIN tim_type tt ON atim.tim_type_id = tt.tim_type_id";
 			// where starting less than 2 hours away
 			selectStatement+= " WHERE atim.tim_start <= SYSDATE + INTERVAL '2' HOUR";
-			// and expiration_date within 24hrs
+			// and expiration_date within 2hrs
 			  selectStatement+= " AND (atim.expiration_date is null OR atim.expiration_date <= SYSDATE + INTERVAL '2' HOUR)";
-			// check that end time isn't within 24hrs
+			// check that end time isn't within 2hrs
   			selectStatement+= " AND (atim.tim_end is null OR atim.tim_end >= SYSDATE + INTERVAL '2' HOUR)";
 
 			rs = statement.executeQuery(selectStatement);

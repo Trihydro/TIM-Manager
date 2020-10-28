017cb49ed17536dfcbeb18bbec5aef0d4b04928b
root <bpayne@trihydro.com>
2020-10-28 17:46:37 +0000

    HSM Functional test and unit tests

---
+++ cv-data-tasks/src/main/java/com/trihydro/tasks/actions/VerifyHSMFunctional.java
@@ -0,0 +1,114 @@
+package com.trihydro.tasks.actions;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import com.google.gson.Gson;
+import com.trihydro.library.helpers.EmailHelper;
+import com.trihydro.library.helpers.Utility;
+import com.trihydro.library.service.RestTemplateProvider;
+import com.trihydro.tasks.config.DataTasksConfiguration;
+import com.trihydro.tasks.models.SignTimModel;
+import com.trihydro.tasks.models.hsmresponse.HsmResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VerifyHSMFunctional implements Runnable {
+    private DataTasksConfiguration config;
+    private Utility utility;
+    private RestTemplateProvider restTemplateProvider;
+    private EmailHelper mailHelper;
+
+    public Date errorLastSent = null;
+    private SignTimModel signTimModel;
+    private HttpEntity<String> entity;
+
+    private static final int sigValidityOverride = 36000000;
+    private static final String message = "AB+AhHAVP6+uw89IRtyVmrEPd12bAwnCgIt80Vzq0of/+T8inWB9AKEH+c036tNAxbX6cGotYRIjiMxbOb9KvfZM2jRlAAAAABQEW+aK51aUP95n4AEIUWd7wquPelB+HceDETPJJJ/o+hfPUkhdHQFjWSIKz4TapJENJz/9BBCQAgCe7rs2AA==";
+
+    @Autowired
+    public void InjectDependencies(DataTasksConfiguration _configuration, Utility _utility,
+            RestTemplateProvider _restTemplateProvider, EmailHelper _emailHelper) {
+        config = _configuration;
+        utility = _utility;
+        restTemplateProvider = _restTemplateProvider;
+        mailHelper = _emailHelper;
+
+        signTimModel = new SignTimModel();
+        signTimModel.setMessage(message);
+        signTimModel.setSigValidity(sigValidityOverride);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        Gson gson = new Gson();
+        String timToSendJson = gson.toJson(signTimModel);
+        entity = new HttpEntity<String>(timToSendJson, headers);
+    }
+
+    public void run() {
+        utility.logWithDate("Running...", this.getClass());
+        try {
+            // ping HSM
+            var response = restTemplateProvider.GetRestTemplate().exchange(config.getHsmUrl() + "/signtim/",
+                    HttpMethod.POST, entity, HsmResponse.class);
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                if (errorLastSent != null) {
+                    // send an email telling us its back up
+                    String email = "HSM Functional Tester was successful in attempting to sign a TIM";
+                    mailHelper.SendEmail(config.getAlertAddresses(), null, "HSM Back Up", email, config.getMailPort(),
+                            config.getMailHost(), config.getFromEmail());
+                }
+                errorLastSent = null;
+            } else if (shouldSendEmail(errorLastSent)) {
+                // got an error, email the team
+                errorLastSent = new Date();
+                String email = "HSM Functional Tester encountered an error while attempting to sign a TIM. The response from the HSM at ";
+                email += config.getHsmUrl() + "/signtim/";
+                email += " was Http Status " + response.getStatusCodeValue();
+                email += ". The body of the response is as follows: ";
+                email += "<br/><br/>";
+                email += response.getBody();
+
+                mailHelper.SendEmail(config.getAlertAddresses(), null, "HSM Error", email, config.getMailPort(),
+                        config.getMailHost(), config.getFromEmail());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // don't rethrow error, or the task won't be reran until the service is
+            // restarted.
+
+            try {
+                String email = "HSM Functional Tester encountered an exception while attempting to sign a TIM:";
+                email += "<br/><br/>";
+                email += e.getMessage();
+                mailHelper.SendEmail(config.getAlertAddresses(), null, "HSM Error", email, config.getMailPort(),
+                        config.getMailHost(), config.getFromEmail());
+            } catch (Exception subEx) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean shouldSendEmail(Date lastSent) {
+        if (lastSent == null) {
+            return true;
+        }
+
+        var currentDate = new Date();
+        var diffInMillis = Math.abs(currentDate.getTime() - lastSent.getTime());
+        var diffInMins = TimeUnit.MINUTES.convert(diffInMillis, TimeUnit.MILLISECONDS);
+        if (diffInMins > config.getHsmErrorEmailFrequencyMinutes()) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ cv-data-tasks/src/test/java/com/trihydro/tasks/actions/VerifyHSMFunctionalTest.java
@@ -0,0 +1,132 @@
+package com.trihydro.tasks.actions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.mail.MessagingException;
+
+import com.trihydro.library.helpers.EmailHelper;
+import com.trihydro.library.helpers.Utility;
+import com.trihydro.library.service.RestTemplateProvider;
+import com.trihydro.tasks.config.DataTasksConfiguration;
+import com.trihydro.tasks.models.hsmresponse.HsmResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.web.client.RestTemplate;
+
+@ExtendWith(MockitoExtension.class)
+public class VerifyHSMFunctionalTest {
+    @Mock
+    private DataTasksConfiguration mockConfig;
+    @Mock
+    private Utility mockUtility;
+    @Mock
+    private RestTemplateProvider mockRestTemplateProvider;
+    @Mock
+    private RestTemplate mockRestTemplate;
+    @Mock
+    private EmailHelper mockEmailHelper;
+    @Mock
+    private ResponseEntity<HsmResponse> mockResponseEntity;
+
+    @InjectMocks
+    private VerifyHSMFunctional uut;
+
+    @BeforeEach
+    public void setup() {
+        doReturn(mockRestTemplate).when(mockRestTemplateProvider).GetRestTemplate();
+        doReturn(mockResponseEntity).when(mockRestTemplate).exchange(any(String.class), any(HttpMethod.class),
+                Mockito.<HttpEntity<String>>any(), Mockito.<Class<HsmResponse>>any());
+    }
+
+    @Test
+    public void cleanupActiveTims_runTest_OK() {
+        // Arrange
+        doReturn(HttpStatus.OK).when(mockResponseEntity).getStatusCode();
+
+        // Act
+        uut.run();
+
+        // assert exchange called
+        verify(mockRestTemplate).exchange(any(String.class), any(HttpMethod.class), Mockito.<HttpEntity<String>>any(),
+                Mockito.<Class<HsmResponse>>any());
+        verifyNoInteractions(mockEmailHelper);
+    }
+
+    @Test
+    public void cleanupActiveTims_runTest_OK_PrevErr() throws MailException, MessagingException {
+        // Arrange
+        doReturn(HttpStatus.OK).when(mockResponseEntity).getStatusCode();
+        uut.errorLastSent = new Date();
+
+        // Act
+        uut.run();
+
+        // assert exchange called
+        verify(mockRestTemplate).exchange(any(String.class), any(HttpMethod.class), Mockito.<HttpEntity<String>>any(),
+                Mockito.<Class<HsmResponse>>any());
+        String email = "HSM Functional Tester was successful in attempting to sign a TIM";
+        verify(mockEmailHelper).SendEmail(mockConfig.getAlertAddresses(), null, "HSM Back Up", email,
+                mockConfig.getMailPort(), mockConfig.getMailHost(), mockConfig.getFromEmail());
+    }
+
+    @Test
+    public void cleanupActiveTims_runTest_Err_NullSendDate() throws MailException, MessagingException {
+        // Arrange
+        doReturn(HttpStatus.I_AM_A_TEAPOT).when(mockResponseEntity).getStatusCode();
+        doReturn(HttpStatus.I_AM_A_TEAPOT.value()).when(mockResponseEntity).getStatusCodeValue();
+
+        // Act
+        uut.run();
+
+        // Assert
+        verify(mockRestTemplate).exchange(any(String.class), any(HttpMethod.class), Mockito.<HttpEntity<String>>any(),
+                Mockito.<Class<HsmResponse>>any());
+        String email = "HSM Functional Tester encountered an error while attempting to sign a TIM. The response from the HSM at ";
+        email += mockConfig.getHsmUrl() + "/signtim/";
+        email += " was Http Status " + HttpStatus.I_AM_A_TEAPOT.value();
+        email += ". The body of the response is as follows: ";
+        email += "<br/><br/>";
+        email += mockResponseEntity.getBody();
+
+        verify(mockEmailHelper).SendEmail(mockConfig.getAlertAddresses(), null, "HSM Error", email,
+                mockConfig.getMailPort(), mockConfig.getMailHost(), mockConfig.getFromEmail());
+    }
+
+    @Test
+    public void cleanupActiveTims_runTest_Err_SendDateWithinPeriod() throws MailException, MessagingException {
+        // Arrange
+        doReturn(HttpStatus.I_AM_A_TEAPOT).when(mockResponseEntity).getStatusCode();
+        doReturn(10).when(mockConfig).getHsmErrorEmailFrequencyMinutes();// only send email within 10 mins
+        // set date to 5 minutes ago
+        var lastSent = Calendar.getInstance();
+        lastSent.setTime(new Date());
+        lastSent.add(Calendar.MINUTE, -5);
+        uut.errorLastSent = lastSent.getTime();
+
+        // Act
+        uut.run();
+
+        // Assert
+        verify(mockRestTemplate).exchange(any(String.class), any(HttpMethod.class), Mockito.<HttpEntity<String>>any(),
+                Mockito.<Class<HsmResponse>>any());
+        verifyNoInteractions(mockEmailHelper);
+    }
+}

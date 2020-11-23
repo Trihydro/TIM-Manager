6f5b406dfa4240c494996f1fb40ba277bd32fd96
Brandon Payne <bpayne@trihydro.com>
2020-11-23 18:44:55 +0000

    Removing unnecessary code

--- cv-data-service-library/src/main/java/com/trihydro/library/helpers/RestTemplateResponseErrorHandler.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/helpers/RestTemplateResponseErrorHandler.java
@@ -9,29 +9,17 @@ import org.springframework.web.client.ResponseErrorHandler;
 
 @Component
 public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
- 
-        @Override
-        public boolean hasError(ClientHttpResponse httpResponse) 
-          throws IOException {
-     
-            return (
-              httpResponse.getStatusCode().series() == HttpStatus.Series.CLIENT_ERROR 
-              || httpResponse.getStatusCode().series() == HttpStatus.Series.SERVER_ERROR);
-        }
-     
-        @Override
-        public void handleError(ClientHttpResponse httpResponse) 
-          throws IOException {
-     
-            if (httpResponse.getStatusCode()
-              .series() == HttpStatus.Series.SERVER_ERROR) {
-                // handle SERVER_ERROR
-            } else if (httpResponse.getStatusCode()
-              .series() == HttpStatus.Series.CLIENT_ERROR) {
-                // handle CLIENT_ERROR
-                if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-                    
-                }
-            }
-        }
+
+    @Override
+    public boolean hasError(ClientHttpResponse httpResponse) throws IOException {
+
+        return (httpResponse.getStatusCode().series() == HttpStatus.Series.CLIENT_ERROR
+                || httpResponse.getStatusCode().series() == HttpStatus.Series.SERVER_ERROR);
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse httpResponse) throws IOException {
+        // allow all responses to pass through to client by not throwing errors here
+        // in the future we may want to handle certain error types at a global level
+    }
 }

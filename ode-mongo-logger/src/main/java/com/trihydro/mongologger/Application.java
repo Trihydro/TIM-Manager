b64dd8c50191b12e165107ce02818c9655cb7642
Brandon Payne <bpayne@trihydro.com>
2020-12-01 21:24:24 +0000

    Importing required classes

--- ode-mongo-logger/src/main/java/com/trihydro/mongologger/Application.java
+++ ode-mongo-logger/src/main/java/com/trihydro/mongologger/Application.java
@@ -1,5 +1,8 @@
 package com.trihydro.mongologger;
 
+import com.trihydro.library.helpers.EmailHelper;
+import com.trihydro.library.helpers.JavaMailSenderImplProvider;
+import com.trihydro.library.helpers.Utility;
 import com.trihydro.mongologger.app.MongoLoggerConfiguration;
 
 import org.springframework.boot.SpringApplication;
@@ -7,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 
+@Import({ Utility.class, EmailHelper.class, JavaMailSenderImplProvider.class })
 @SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 @EnableConfigurationProperties(MongoLoggerConfiguration.class)
 public class Application {

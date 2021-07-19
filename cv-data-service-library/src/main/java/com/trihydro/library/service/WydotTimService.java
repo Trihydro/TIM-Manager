534798aaf3d102c250881956144844f8589a25d6
Trevor Trouchon <ttrouchon@trihydro.com>
2021-07-19 21:16:30 +0000

    feat: add /buffer-tims endpoint. Update RW TIM deletion logic to delete buffers too.

--- cv-data-service-library/src/main/java/com/trihydro/library/service/WydotTimService.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/service/WydotTimService.java
@@ -407,10 +407,20 @@ public class WydotTimService {
     }
 
     public boolean clearTimsById(String timTypeStr, String clientId, String direction) {
+        return clearTimsById(timTypeStr, clientId, direction, false);
+    }
+
+    public boolean clearTimsById(String timTypeStr, String clientId, String direction, boolean isRwTim) {
 
         List<ActiveTim> activeTims = new ArrayList<ActiveTim>();
         TimType timType = getTimType(timTypeStr);
-        activeTims = activeTimService.getActiveTimsByClientIdDirection(clientId, timType.getTimTypeId(), direction);
+        activeTims
+                .addAll(activeTimService.getActiveTimsByClientIdDirection(clientId, timType.getTimTypeId(), direction));
+
+        if (isRwTim) {
+            activeTims.addAll(activeTimService.getBufferTimsByClientId(clientId));
+        }
+
         utility.logWithDate(activeTims.size() + " active_tim found for deletion");
 
         deleteTimsFromRsusAndSdx(activeTims);

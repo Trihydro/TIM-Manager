2d9992256c4efa9c849032b29bddb389f5f0dc57
Brandon Payne <bpayne@trihydro.com>
2021-07-30 15:20:41 +0000

    Updating  buffer for swagger documentation

--- cv-data-service-library/src/main/java/com/trihydro/library/model/Buffer.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/model/Buffer.java
@@ -1,7 +1,11 @@
 package com.trihydro.library.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Buffer {
 	private Double distance;
+	
+	@ApiModelProperty(value = "Expected values are leftClosed, rightClosed, workers, surfaceGravel, surfaceMilled, surfaceDirt, delay, prepareStop")
 	private String action;
 
 	public Buffer() {
@@ -17,6 +21,7 @@ public class Buffer {
 		return this.distance;
 	}
 
+	@ApiModelProperty(hidden = true)
 	public Double getDistanceMeters() {
 		return this.distance * 1609.34;
 

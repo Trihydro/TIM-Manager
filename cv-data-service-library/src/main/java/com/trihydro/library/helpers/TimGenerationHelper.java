84bbeb178913a2a56888fad2fa94f175ea645904
Brandon Payne <bpayne@trihydro.com>
2024-03-27 16:03:50 -0600

    Refresh update to calculate nodell instead of pulling from db

--- cv-data-service-library/src/main/java/com/trihydro/library/helpers/TimGenerationHelper.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/helpers/TimGenerationHelper.java
@@ -782,15 +782,17 @@ public class TimGenerationHelper {
 
         if (aTim.getPathId() != null) {
             NodeXY[] nodes = pathNodeLLService.getNodeLLForPath(aTim.getPathId());
-            // Periodically we see a mismatch in our node-LL# from the database and what
-            // the ODE thinks it should use. As a result, this errs out if we specify the
-            // wrong number. To combat this, just reset to node-LL
-            for (NodeXY node : nodes) {
-                node.setDelta("node-LL");
-            }
-            if (nodes == null || nodes.length == 0) {
-                nodes = buildNodePathFromMileposts(mps, anchor);
-            }
+            // // Periodically we see a mismatch in our node-LL# from the database and what
+            // // the ODE thinks it should use. As a result, this errs out if we specify the
+            // // wrong number. To combat this, just reset to node-LL
+            // for (NodeXY node : nodes) {
+            // node.setDelta("node-LL");
+            // }
+            // if (nodes == null || nodes.length == 0) {
+            // }
+            // 3/27/24, nodell doesn't have an order so refreshing is causing major issues
+            // instead, we will just generate the path from the mileposts
+            nodes = buildNodePathFromMileposts(mps, anchor);
             Path path = new Path();
             path.setScale(0);
             path.setType("ll");// offset path is now standard

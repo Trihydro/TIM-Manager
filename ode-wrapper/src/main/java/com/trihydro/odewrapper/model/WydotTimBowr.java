75d095289726839a1c57ca7ddb38bdcd1dd1850a
dmccoystephenson <dmccoystephenson@gmail.com>
2024-07-08 15:05:16 -0600

    Removed class variables in `WydotTimBowr` which are present in parent class

--- ode-wrapper/src/main/java/com/trihydro/odewrapper/model/WydotTimBowr.java
+++ ode-wrapper/src/main/java/com/trihydro/odewrapper/model/WydotTimBowr.java
@@ -7,23 +7,8 @@ import io.swagger.annotations.ApiModelProperty;
 
 public class WydotTimBowr extends WydotTim {
 
-    @ApiModelProperty(value = "Expected values are I, D, B", required = true)
-	private String direction;
-
     @ApiModelProperty(value = "Expected value is BlowOverWeightRestriction", required = true)
 	private String type;
-    
-    @ApiModelProperty(value = "The common name for the selected route", required = true)
-	private String route;
-
-    @ApiModelProperty(required = true)
-	private String clientId;
-
-    @ApiModelProperty(required = true)
-	private Coordinate startPoint;
-
-	@ApiModelProperty(required = true)
-	private Coordinate endPoint;
     
 	@ApiModelProperty(required = false)
 	private String startDateTime;
@@ -40,12 +25,7 @@ public class WydotTimBowr extends WydotTim {
 
     public WydotTimBowr(WydotTimBowr o) {
         super(o);
-        this.direction = o.direction;
         this.type = o.type;
-        this.route = o.route;
-        this.clientId = o.clientId;
-        this.startPoint = o.startPoint;
-        this.endPoint = o.endPoint;
         this.startDateTime = o.startDateTime;
         this.endDateTime = o.endDateTime;
         this.data = o.data;
@@ -55,52 +35,12 @@ public class WydotTimBowr extends WydotTim {
         return new WydotTimBowr(this);
     }
 
-    public String getDirection() {
-        return direction;
-    }
-
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
-
     public String getType() {
         return type;
     }
 
     public void setType(String type) {
         this.type = type;
-    }
-
-    public String getRoute() {
-        return route;
-    }
-
-    public void setRoute(String route) {
-        this.route = route;
-    }
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public Coordinate getStartPoint() {
-        return startPoint;
-    }
-
-    public void setStartPoint(Coordinate startPoint) {
-        this.startPoint = startPoint;
-    }
-
-    public Coordinate getEndPoint() {
-        return endPoint;
-    }
-
-    public void setEndPoint(Coordinate endPoint) {
-        this.endPoint = endPoint;
     }
 
     public String getStartDateTime() {

33e6a1f69bd9aa7e2dafdf5c9ede3aa5f2ad6543
dmccoystephenson <dmccoystephenson@gmail.com>
2024-09-09 14:37:55 -0600

    Added teardown scripts for postgres database

---
+++ db-scripts/pgsql/teardown/sql/3-drop_all_functions.sql
@@ -0,0 +1,4 @@
+drop function if exists milepost_cn_lu cascade;
+drop function if exists milepost_rc_lu cascade;
+drop function if exists route_name cascade;
+drop function if exists rsu_route cascade;

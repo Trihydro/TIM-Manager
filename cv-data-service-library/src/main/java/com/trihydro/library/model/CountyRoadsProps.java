fe36a441d4cdd0d23978368df0b01d7b61ee44dd
dmccoystephenson <dmccoystephenson@gmail.com>
2024-09-20 15:20:08 -0600

    Made county roads geometry view name configurable

--- cv-data-service-library/src/main/java/com/trihydro/library/model/CountyRoadsProps.java
+++ cv-data-service-library/src/main/java/com/trihydro/library/model/CountyRoadsProps.java
@@ -3,4 +3,7 @@ package com.trihydro.library.model;
 public interface CountyRoadsProps {
     String getCountyRoadsTriggerViewName();
     void setCountyRoadsTriggerViewName(String countyRoadsTriggerViewName);
+    String getCountyRoadsGeometryViewName();
+    void setCountyRoadsGeometryViewName(String countyRoadsGeometryViewName);
+    
 }

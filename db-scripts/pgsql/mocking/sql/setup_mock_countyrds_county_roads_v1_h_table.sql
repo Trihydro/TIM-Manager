a4c279b52d79cdaa6d075ce17754ba08363deb32
dmccoystephenson <dmccoystephenson@gmail.com>
2024-09-28 12:20:41 -0600

    Added database trigger to notify adhoc_listener_channel about changes to target table

--- db-scripts/pgsql/mocking/sql/setup_mock_countyrds_county_roads_v1_h_table.sql
+++ db-scripts/pgsql/mocking/sql/setup_mock_countyrds_county_roads_v1_h_table.sql
@@ -49,4 +49,25 @@ CREATE TABLE countyrds.county_roads_v1_h (
 INSERT INTO countyrds.county_roads_v1_h (objectid, globalid, cr_id, authority_organization_id, county, type, rd_no, name, description, point_from, point_to, active, closed, c2lhpv, ntt, info_last_updated_user, conditions_last_updated_date, gdb_from_date, gdb_to_date, gdb_archive_old, shape) VALUES
 (1, '1', 1, 1, 'county', 'type', 'rd_no', 'name', 'description', 'point_from', 'point_to', 1, 0, 0, 0, 'info_last_updated_user', '2020-01-01 00:00:00', '2020-01-01 00:00:00', '2020-01-01 00:00:00', 0, 'POINT(0 0)'),
 (2, '2', 2, 2, 'county', 'type', 'rd_no', 'name', 'description', 'point_from', 'point_to', 1, 0, 0, 0, 'info_last_updated_user', '2020-01-01 00:00:00', '2020-01-01 00:00:00', '2020-01-01 00:00:00', 0, 'POINT(0 0)'),
-(3, '3', 3, 3, 'county', 'type', 'rd_no', 'name', 'description', 'point_from', 'point_to', 1, 0, 0, 0, 'info_last_updated_user', '2020-01-01 00:00:00', '2020-01-01 00:00:00', '2020-01-01 00:00:00', 0, 'POINT(0 0)')
+(3, '3', 3, 3, 'county', 'type', 'rd_no', 'name', 'description', 'point_from', 'point_to', 1, 0, 0, 0, 'info_last_updated_user', '2020-01-01 00:00:00', '2020-01-01 00:00:00', '2020-01-01 00:00:00', 0, 'POINT(0 0)');
+
+-- create database trigger that calls pg_notify with a custom payload that includes row data
+CREATE OR REPLACE FUNCTION notify_county_roads_v1_h() RETURNS trigger AS $$
+DECLARE
+    payload json;
+BEGIN
+    payload := json_build_object(
+        'table', TG_TABLE_NAME,
+        'action', TG_OP,
+        'before', row_to_json(OLD),
+        'after', row_to_json(NEW)
+    );
+    PERFORM pg_notify('adhoc_listener_channel', payload::text);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS notify_county_roads_v1_h ON countyrds.county_roads_v1_h;
+CREATE TRIGGER notify_county_roads_v1_h
+AFTER INSERT OR UPDATE OR DELETE ON countyrds.county_roads_v1_h
+FOR EACH ROW EXECUTE FUNCTION notify_county_roads_v1_h();

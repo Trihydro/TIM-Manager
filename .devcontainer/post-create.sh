481eefd82e9d305d5b25fe3f06422bcec290a27c
Marc Wodahl <mwodahl@trihydro.com>
2024-12-02 12:26:17 -0700

    Remove unused mvn install commands from post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,10 +7,6 @@ WYOCV_RES_DIR=/workspaces/${REPO_NAME}/resources
 echo "WYOCV_RES_DIR: ${WYOCV_RES_DIR}"
 
 # Install jpo-ode JARs to maven repository
-mvn install:install-file -Dfile="${WYOCV_RES_DIR}/jpo-ode-plugins-2.0.0-SNAPSHOT.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-plugins -Dversion="2.0.0-SNAPSHOT" -Dpackaging=jar
-mvn install:install-file -Dfile="${WYOCV_RES_DIR}/jpo-ode-core-2.0.0-SNAPSHOT.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-core -Dversion="2.0.0-SNAPSHOT" -Dpackaging=jar
-mvn install:install-file -Dfile="${WYOCV_RES_DIR}/jpo-ode-common-2.0.0-SNAPSHOT.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-common -Dversion="2.0.0-SNAPSHOT" -Dpackaging=jar
-mvn install:install-file -Dfile="${WYOCV_RES_DIR}/jpo-ode-svcs.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-svcs -Dversion="2.0.0-SNAPSHOT" -Dpackaging=jar
 mvn install:install-file -Dfile="${WYOCV_RES_DIR}/ucp.jar" -DgroupId="com.oracle" -DartifactId=ucp -Dversion="12.2.0.1.0" -Dpackaging=jar
 mvn install:install-file -Dfile="${WYOCV_RES_DIR}/postgresql-42.6.0.jar" -DgroupId="org.postgresql" -DartifactId=postgresql -Dversion="42.6.0" -Dpackaging=jar
 

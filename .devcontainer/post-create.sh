2d4b0d1b534aafa5326cc2c21a599961c7bb1ad4
Marc Wodahl <mwodahl@trihydro.com>
2024-12-05 07:53:30 -0700

    Resolve repository name dynamically in devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,10 @@
-WYOCV_RES_DIR=/workspaces/WyoCV/resources
+# Please note that the following must be set to the name of the
+# repository for the devcontainer to build and run correctly
+REPO_PATH=$(dirname $(dirname $(realpath $0)))
+REPO_NAME=$(basename ${REPO_PATH})
+WYOCV_RES_DIR=/workspaces/${REPO_NAME}/resources
+
+echo "WYOCV_RES_DIR: ${WYOCV_RES_DIR}"
 
 # Install jpo-ode JARs to maven repository
 mvn install:install-file -Dfile="${WYOCV_RES_DIR}/jpo-ode-plugins-2.0.0-SNAPSHOT.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-plugins -Dversion="2.0.0-SNAPSHOT" -Dpackaging=jar
@@ -7,6 +13,10 @@ mvn install:install-file -Dfile="${WYOCV_RES_DIR}/jpo-ode-common-2.0.0-SNAPSHOT.
 mvn install:install-file -Dfile="${WYOCV_RES_DIR}/jpo-ode-svcs.jar" -DgroupId="us.dot.jpo.ode" -DartifactId=jpo-ode-svcs -Dversion="2.0.0-SNAPSHOT" -Dpackaging=jar
 mvn install:install-file -Dfile="${WYOCV_RES_DIR}/ucp.jar" -DgroupId="com.oracle" -DartifactId=ucp -Dversion="12.2.0.1.0" -Dpackaging=jar
 mvn install:install-file -Dfile="${WYOCV_RES_DIR}/postgresql-42.6.0.jar" -DgroupId="org.postgresql" -DartifactId=postgresql -Dversion="42.6.0" -Dpackaging=jar
+
+# Install python
+apt update
+apt install -y python3 python3-pip
 
 # Starting in vscode 1.42.0, there appears to be an issue w/ git authentication
 # Workaround until https://github.com/microsoft/vscode-remote-release/issues/2340 is resolved:

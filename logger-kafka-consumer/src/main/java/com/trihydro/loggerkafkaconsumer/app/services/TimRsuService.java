c567997cb5b7c3c27057b6d272b30fceccf1c29a
dmccoystephenson <dmccoystephenson@gmail.com>
2025-03-14 13:58:43 -0600

    Removed unused import in TimRsuService class

--- logger-kafka-consumer/src/main/java/com/trihydro/loggerkafkaconsumer/app/services/TimRsuService.java
+++ logger-kafka-consumer/src/main/java/com/trihydro/loggerkafkaconsumer/app/services/TimRsuService.java
@@ -3,7 +3,6 @@ package com.trihydro.loggerkafkaconsumer.app.services;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
 
 import com.trihydro.library.helpers.SQLNullHandler;
 import com.trihydro.library.tables.TimDbTables;

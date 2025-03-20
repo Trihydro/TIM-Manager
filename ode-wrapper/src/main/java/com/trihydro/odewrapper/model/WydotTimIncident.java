3ff0bf59d4ae6935f673a268a1ab43a10ca66c1e
dmccoystephenson <dmccoystephenson@gmail.com>
2025-03-20 16:59:49 -0600

    Formatted WydotTimIncident class

--- ode-wrapper/src/main/java/com/trihydro/odewrapper/model/WydotTimIncident.java
+++ ode-wrapper/src/main/java/com/trihydro/odewrapper/model/WydotTimIncident.java
@@ -10,112 +10,114 @@ import io.swagger.annotations.ApiModelProperty;
 
 public class WydotTimIncident extends WydotTim {
 
-    @ApiModelProperty(value = "Expected values are mudslide, livestock, avalanche, avalancheControl, landslide, wildfire, signInstall, mowing")
-    private String problem;
-    private String effect;
+  @ApiModelProperty(value = "Expected values are mudslide, livestock, avalanche, avalancheControl, landslide, wildfire, signInstall, mowing")
+  private String problem;
+  private String effect;
 
-    @ApiModelProperty(value = "Expected values are caution, delays, stop")
-    private String action;
-    @ApiModelProperty(required = true)
-    private String incidentId;
-    @ApiModelProperty(required = true)
-    private String highway;
+  @ApiModelProperty(value = "Expected values are caution, delays, stop")
+  private String action;
+  @ApiModelProperty(required = true)
+  private String incidentId;
+  @ApiModelProperty(required = true)
+  private String highway;
 
-    private Integer pk;
-    @ApiModelProperty(hidden = true)
-    private transient String clientId;
-    @ApiModelProperty(hidden = true)
-    private transient String route;
-    @ApiModelProperty(hidden = true)
-    private transient List<String> itisCodes;
+  private Integer pk;
+  @ApiModelProperty(hidden = true)
+  private transient String clientId;
+  @ApiModelProperty(hidden = true)
+  private transient String route;
+  @ApiModelProperty(hidden = true)
+  private transient List<String> itisCodes;
 
-    @ApiModelProperty(value = "Optional. If not provided, a TIM will be generated extending 1 mile upstream from the startPoint", required = false)
-    private transient Coordinate endPoint;
+  @ApiModelProperty(value = "Optional. If not provided, a TIM will be generated extending 1 mile upstream from the startPoint", required = false)
+  private transient Coordinate endPoint;
 
-    @ApiModelProperty()
-    private transient String problemOtherText;
+  @ApiModelProperty()
+  private transient String problemOtherText;
 
-    public WydotTimIncident() {
+  public WydotTimIncident() {
 
+  }
+
+  public WydotTimIncident(WydotTimIncident o) {
+    super(o);
+    this.problem = o.problem;
+    this.effect = o.effect;
+    this.action = o.action;
+    this.incidentId = o.incidentId;
+    this.highway = o.highway;
+    this.pk = o.pk;
+    this.clientId = o.clientId;
+    this.route = o.route;
+
+    if (o.itisCodes != null) {
+      this.itisCodes = new ArrayList<>(o.itisCodes);
+    }
+    if (o.endPoint != null) {
+      this.endPoint = new Coordinate(o.endPoint.getLatitude(), o.endPoint.getLongitude());
     }
 
-    public WydotTimIncident(WydotTimIncident o) {
-        super(o);
-        this.problem = o.problem;
-        this.effect = o.effect;
-        this.action = o.action;
-        this.incidentId = o.incidentId;
-        this.highway = o.highway;
-        this.pk = o.pk;
-        this.clientId = o.clientId;
-        this.route = o.route;
+    this.problemOtherText = o.problemOtherText;
+  }
 
-        if (o.itisCodes != null)
-            this.itisCodes = new ArrayList<>(o.itisCodes);
-        if (o.endPoint != null)
-            this.endPoint = new Coordinate(o.endPoint.getLatitude(), o.endPoint.getLongitude());
+  @Override
+  public WydotTimIncident copy() {
+    return new WydotTimIncident(this);
+  }
 
-        this.problemOtherText = o.problemOtherText;
-    }
+  public Integer getPk() {
+    return this.pk;
+  }
 
-    @Override
-    public WydotTimIncident copy() {
-        return new WydotTimIncident(this);
-    }
+  public void setPk(Integer pk) {
+    this.pk = pk;
+  }
 
-    public Integer getPk() {
-        return this.pk;
-    }
+  public String getHighway() {
+    return this.highway;
+  }
 
-    public void setPk(Integer pk) {
-        this.pk = pk;
-    }
+  public void setHighway(String highway) {
+    this.highway = highway;
+  }
 
-    public String getHighway() {
-        return this.highway;
-    }
+  public String getIncidentId() {
+    return this.incidentId;
+  }
 
-    public void setHighway(String highway) {
-        this.highway = highway;
-    }
+  public void setIncidentId(String incidentId) {
+    this.incidentId = incidentId;
+  }
 
-    public String getIncidentId() {
-        return this.incidentId;
-    }
+  public String getProblem() {
+    return this.problem;
+  }
 
-    public void setIncidentId(String incidentId) {
-        this.incidentId = incidentId;
-    }
+  public void setProblem(String problem) {
+    this.problem = problem;
+  }
 
-    public String getProblem() {
-        return this.problem;
-    }
+  public String getEffect() {
+    return this.effect;
+  }
 
-    public void setProblem(String problem) {
-        this.problem = problem;
-    }
+  public void setEffect(String effect) {
+    this.effect = effect;
+  }
 
-    public String getEffect() {
-        return this.effect;
-    }
+  public String getAction() {
+    return this.action;
+  }
 
-    public void setEffect(String effect) {
-        this.effect = effect;
-    }
+  public void setAction(String action) {
+    this.action = action;
+  }
 
-    public String getAction() {
-        return this.action;
-    }
+  public String getProblemOtherText() {
+    return this.problemOtherText;
+  }
 
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    public String getProblemOtherText() {
-        return this.problemOtherText;
-    }
-
-    public void setProblemOtherText(String problemOtherText) {
-        this.problemOtherText = problemOtherText;
-    }
+  public void setProblemOtherText(String problemOtherText) {
+    this.problemOtherText = problemOtherText;
+  }
 }

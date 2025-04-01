9c104deb7853758833b19b0e76da00d367db937c
Matt Cook <matt.cook@trihydro.com>
2025-04-01 16:00:26 -0600

    refactor: use top level try-catch for clearer code execution paths

--- cv-data-tasks/src/main/java/com/trihydro/tasks/actions/CleanupStaleActiveTimHoldingRecords.java
+++ cv-data-tasks/src/main/java/com/trihydro/tasks/actions/CleanupStaleActiveTimHoldingRecords.java
@@ -41,65 +41,68 @@ public class CleanupStaleActiveTimHoldingRecords implements Runnable {
      */
     @Override
     public void run() {
-        log.info("Running...");
+        try {
+            log.info("Running...");
 
-        // Retrieve all active_tim_holding records
-        List<ActiveTimHolding> currentRecords = retrieveAllActiveTimHoldingRecords();
-        log.info("Retrieved {} active_tim_holding records", currentRecords.size());
+            // Retrieve all active_tim_holding records
+            List<ActiveTimHolding> currentRecords = retrieveAllActiveTimHoldingRecords();
+            log.info("Retrieved {} active_tim_holding records", currentRecords.size());
 
-        // If no stale records identified last run, this could be the first time the task runs, so add all active_tim_holding records to staleRecords set
-        if (staleRecordsIdentifiedLastRun.isEmpty()) {
-            log.info("No stale records identified last run. Adding {} active_tim_holding records to staleRecords set for next run", currentRecords.size());
+            // If no stale records identified last run, this could be the first time the task runs, so add all active_tim_holding records to staleRecords set
+            if (staleRecordsIdentifiedLastRun.isEmpty()) {
+                log.info("No stale records identified last run. Adding {} active_tim_holding records to staleRecords set for next run", currentRecords.size());
+                for (ActiveTimHolding record : currentRecords) {
+                    staleRecordsIdentifiedLastRun.add(record.getActiveTimHoldingId());
+                }
+                return;
+            }
+
+            // Separate likely stale active_tim_holding records
+            List<ActiveTimHolding> likelyStaleRecords = new ArrayList<>();
+            List<ActiveTimHolding> newRecords = new ArrayList<>();
             for (ActiveTimHolding record : currentRecords) {
+                if (staleRecordsIdentifiedLastRun.contains(record.getActiveTimHoldingId())) {
+                    likelyStaleRecords.add(record);
+                } else {
+                    newRecords.add(record);
+                }
+            }
+            log.info("Identified {} likely stale active_tim_holding records", likelyStaleRecords.size());
+
+            // Check for active_tim records
+            List<ActiveTim> activeTims = retrieveAllActiveTimRecords();
+            HashMap<String, List<Long>> activeTimIdsByClientId = mapActiveTimIdsByClientId(activeTims);
+            List<Long> activeTimIdsToDelete = new ArrayList<>();
+            for (ActiveTimHolding record : likelyStaleRecords) {
+                List<Long> activeTimIds = activeTimIdsByClientId.get(record.getClientId());
+                if (activeTimIds == null) {
+                    continue;
+                }
+                activeTimIdsToDelete.addAll(activeTimIds);
+                // TODO: Delete only if the failed update was to expire the active TIM.
+                // TODO: Consider re-submitting the active TIM if the failed update was not meant to expire it.
+            }
+            removeActiveTimRecords(activeTimIdsToDelete); // active_tim records are no longer up-to-date
+
+
+            // Delete likely stale active_tim_holding records
+            for (ActiveTimHolding record : likelyStaleRecords) {
+                removeActiveTimHoldingRecord(record);
+            }
+            log.info("Deleted {} likely stale active_tim_holding records", likelyStaleRecords.size());
+
+            // Clear staleRecords set
+            log.info("Clearing staleRecords set...");
+            staleRecordsIdentifiedLastRun.clear();
+
+            // Record for future runs
+            for (ActiveTimHolding record : newRecords) {
                 staleRecordsIdentifiedLastRun.add(record.getActiveTimHoldingId());
             }
-            return;
+            log.info("Added {} active_tim_holding records to staleRecords set for next run", newRecords.size());
+        } catch (Exception e) {
+            log.error("Failed to clean up stale active_tim_holding records: {}", e.getMessage(), e);
         }
-
-        // Separate likely stale active_tim_holding records
-        List<ActiveTimHolding> likelyStaleRecords = new ArrayList<>();
-        List<ActiveTimHolding> newRecords = new ArrayList<>();
-        for (ActiveTimHolding record : currentRecords) {
-            if (staleRecordsIdentifiedLastRun.contains(record.getActiveTimHoldingId())) {
-                likelyStaleRecords.add(record);
-            } else {
-                newRecords.add(record);
-            }
-        }
-        log.info("Identified {} likely stale active_tim_holding records", likelyStaleRecords.size());
-
-        // Check for active_tim records
-        List<ActiveTim> activeTims = retrieveAllActiveTimRecords();
-        HashMap<String, List<Long>> activeTimIdsByClientId = mapActiveTimIdsByClientId(activeTims);
-        List<Long> activeTimIdsToDelete = new ArrayList<>();
-        for (ActiveTimHolding record : likelyStaleRecords) {
-            List<Long> activeTimIds = activeTimIdsByClientId.get(record.getClientId());
-            if (activeTimIds == null) {
-                continue;
-            }
-            activeTimIdsToDelete.addAll(activeTimIds);
-            // TODO: Delete only if the failed update was to expire the active TIM.
-            // TODO: Consider re-submitting the active TIM if the failed update was not meant to expire it.
-        }
-        removeActiveTimRecords(activeTimIdsToDelete); // active_tim records are no longer up-to-date
-        log.info("Deleted corresponding active_tim records with ids: ({}), which were outdated (the presence of stale active_tim_holding records indicates a failure to update).",
-            activeTimIdsToDelete);
-
-        // Delete likely stale active_tim_holding records
-        for (ActiveTimHolding record : likelyStaleRecords) {
-            removeActiveTimHoldingRecord(record);
-        }
-        log.info("Deleted {} likely stale active_tim_holding records", likelyStaleRecords.size());
-
-        // Clear staleRecords set
-        log.info("Clearing staleRecords set...");
-        staleRecordsIdentifiedLastRun.clear();
-
-        // Record for future runs
-        for (ActiveTimHolding record : newRecords) {
-            staleRecordsIdentifiedLastRun.add(record.getActiveTimHoldingId());
-        }
-        log.info("Added {} active_tim_holding records to staleRecords set for next run", newRecords.size());
     }
 
     private static HashMap<String, List<Long>> mapActiveTimIdsByClientId(List<ActiveTim> activeTims) {
@@ -113,43 +116,50 @@ public class CleanupStaleActiveTimHoldingRecords implements Runnable {
         return activeTimIdsByClientId;
     }
 
-    private List<ActiveTimHolding> retrieveAllActiveTimHoldingRecords() {
+    private List<ActiveTimHolding> retrieveAllActiveTimHoldingRecords() throws CleanupStaleActiveTimHoldingRecordsException {
         try {
             return activeTimHoldingService.getAllRecords();
         } catch (Exception e) {
-            log.error("Failed to retrieve all active_tim_holding records. Is the cv-data-controller service running?", e);
+            throw new CleanupStaleActiveTimHoldingRecordsException("Failed to retrieve all active_tim_holding records. Is the cv-data-controller service running?", e);
         }
-        return new ArrayList<>();
     }
 
-    private List<ActiveTim> retrieveAllActiveTimRecords() {
+    private List<ActiveTim> retrieveAllActiveTimRecords() throws CleanupStaleActiveTimHoldingRecordsException {
         try {
             return activeTimService.getAllRecords();
         } catch (Exception e) {
-            log.error("Failed to retrieve all active_tim records. Is the cv-data-controller service running?", e);
+            throw new CleanupStaleActiveTimHoldingRecordsException("Failed to retrieve all active_tim records. Is the cv-data-controller service running?", e);
         }
-        return new ArrayList<>();
     }
 
-    private void removeActiveTimRecords(List<Long> activeTimIds) {
+    private void removeActiveTimRecords(List<Long> activeTimIds) throws CleanupStaleActiveTimHoldingRecordsException {
         try {
             boolean success = activeTimService.deleteActiveTimsById(activeTimIds);
             if (!success) {
                 log.error("Failed to delete active_tim records with ids: {}", activeTimIds);
+            } else {
+                log.info("Deleted corresponding active_tim records with ids: ({}), which were outdated (the presence of stale active_tim_holding records indicates a failure to update).",
+                        activeTimIds);
             }
         } catch (Exception e) {
-            log.error("Failed to delete active_tim record with ids: {}. Is the cv-data-controller service running?", activeTimIds, e);
+            throw new CleanupStaleActiveTimHoldingRecordsException(
+                    String.format("Failed to delete active_tim record with ids: %s. Is the cv-data-controller service running?", activeTimIds),
+                    e
+            );
         }
     }
 
-    private void removeActiveTimHoldingRecord(ActiveTimHolding record) {
+    private void removeActiveTimHoldingRecord(ActiveTimHolding record) throws CleanupStaleActiveTimHoldingRecordsException {
         try {
             boolean success = activeTimHoldingService.deleteActiveTimHolding(record.getActiveTimHoldingId());
             if (!success) {
                 log.error("Failed to delete active_tim_holding record with id: {}", record.getActiveTimHoldingId());
             }
         } catch (Exception e) {
-            log.error("Failed to delete active_tim_holding record with id: {}. Is the cv-data-controller service running?", record.getActiveTimHoldingId(), e);
+            throw new CleanupStaleActiveTimHoldingRecordsException(
+                    String.format("Failed to delete active_tim_holding record with id: %d. Is the cv-data-controller service running?", record.getActiveTimHoldingId()),
+                    e
+            );
         }
     }
 
@@ -166,5 +176,11 @@ public class CleanupStaleActiveTimHoldingRecords implements Runnable {
     protected void setStaleRecordsIdentifiedLastRun(Set<Long> staleRecordsIdentifiedLastRun) {
         this.staleRecordsIdentifiedLastRun.clear();
         this.staleRecordsIdentifiedLastRun.addAll(staleRecordsIdentifiedLastRun);
+    }
+
+    private static class CleanupStaleActiveTimHoldingRecordsException extends Exception {
+        public CleanupStaleActiveTimHoldingRecordsException(String message, Throwable cause) {
+            super(message, cause);
+        }
     }
 }
